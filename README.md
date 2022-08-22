1b3c885c0fac2ea9e3822d903b7a9291ac42d38e
Ashu Singh <ashu.ask.singh@gmail.com>
2022-08-22 11:17:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leetcode-practise
+ For my daily LeetCode Practise
---
+++ src/Day001.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Day001 {
+
+    public boolean isPowerOfFour(int n) {
+        while(n !=1){
+            if(n%4!=0){
+                return false;
+            }
+            n=n/4;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Day001 ashu = new Day001();
+        System.out.println(ashu.isPowerOfFour(64));
+    }
+}
