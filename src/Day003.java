fe831cc4296c8a9d764dcf6062f2dc5b1fc47ac6
Ashu Singh <ashu.ask.singh@gmail.com>
2022-08-24 23:50:34 +0530

    day 3 done

---
+++ src/Day003.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Day003 {
+
+    public boolean isPowerOfThree(int n) {
+        if(n==1)
+            return true;
+        if(n < 3)
+            return false;
+
+        while(n !=1){
+            if(n%3!=0){
+                return false;
+            }
+            n=n/4;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Day003 ashu = new Day003();
+        System.out.println(ashu.isPowerOfThree(27));
+    }
+}
