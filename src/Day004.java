005d74e77b597ab21e5218f71b6dd482d441978f
Ashu Singh <ashu.ask.singh@gmail.com>
2022-08-26 00:43:09 +0530

    day 4 done

---
+++ src/Day004.java
@@ -0,0 +1,41 @@
+package src;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day004 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        ArrayList<Character> subSet = new ArrayList();
+        for (int i=0 ; i<ransomNote.length(); i++) {
+            subSet.add(ransomNote.charAt(i));
+        }
+
+        ArrayList<Character> set = new ArrayList();
+        for (int i=0 ; i<magazine.length(); i++) {
+            set.add(magazine.charAt(i));
+        }
+
+//        int length = (ransomNote.length() > magazine.length()? ransomNote.length() : magazine.length());
+
+        for(int i =0 ;i<magazine.length(); i++){
+            subSet.remove(set.get(i));
+
+        }
+
+        if (subSet.size() != 0)
+            return false;
+        return true;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Day004 day004 = new Day004();
+        System.out.println(day004.canConstruct("aa","aabaabb"));
+    }
+}
