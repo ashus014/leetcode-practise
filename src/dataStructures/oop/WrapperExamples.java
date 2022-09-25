bebfc9d829e7e542a71a2a2b44e4db2fa031b930
Ashu Singh <ashu.ask.singh@gmail.com>
2022-09-25 16:52:08 +0530

    working with wrapper class

---
+++ src/dataStructures/oop/WrapperExamples.java
@@ -0,0 +1,10 @@
+package src.dataStructures.oop;
+
+public class WrapperExamples {
+    
+    public static void main(String[] args) {
+        
+        Integer integer = 34;
+        System.out.println(integer.toString());
+    }
+}
