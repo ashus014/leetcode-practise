429c2348add7a2bb6b4d7ab7ae1ede97ed4b5b08
Ashu Singh <ashu.ask.singh@gmail.com>
2022-09-30 00:01:40 +0530

    working with linked list

---
+++ src/dataStructures/linkedList/Main.java
@@ -0,0 +1,16 @@
+package src.dataStructures.linkedList;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        LinkedListDemo lDemo = new LinkedListDemo();
+        lDemo.insertFirst(1);
+        lDemo.insertFirst(2);
+        lDemo.insertFirst(3);
+        lDemo.insertFirst(4);
+
+        lDemo.display();
+
+    }
+}
