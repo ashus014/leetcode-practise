8da5fff8db02f4b622168042eee1dceacfd38420
Ashu Singh <ashu.ask.singh@gmail.com>
2022-10-18 14:04:40 +0530

    more leet code

--- src/DSASheetForDataStructures/Arrays/Question8.java
+++ src/DSASheetForDataStructures/Arrays/Question8.java
@@ -2,24 +2,24 @@ package src.DSASheetForDataStructures.Arrays;
 
 public class Question8 {
     public static void main(String[] args) {
-        int []arr = {1,2,3,-2,5};
-        System.out.println(maxSubarraySum(arr, 5));
+        int []arr = {-2 ,1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubarraySum(arr, 9));
     }
 
     static private long maxSubarraySum(int a[], int n){
-        
-        // Your code here
-        int size = a.length;
-        int max_so_far = Integer.MIN_VALUE, max_ending_here
-                                            = 0;
- 
-        for (int i = 0; i < size; i++) {
-            max_ending_here = max_ending_here + a[i];
-            if (max_so_far < max_ending_here)
-                max_so_far = max_ending_here;
-            if (max_ending_here < 0)
-                max_ending_here = 0;
+        long result = Long.MIN_VALUE, sum=0;
+
+        for(int i=0; i<n; i++) {
+            sum = 0;
+            for(int j=i; j<n; j++) {
+                sum += a[j];
+
+                if(sum > result) {
+                    result = sum;
+                }
+            }
         }
-        return max_so_far;
+
+        return result;
     }
 }
