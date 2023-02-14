bbd857a3b816604df240f11ace995f7629f4fff4
Ashu <ashu.ask.singh@gmail.com>
2023-02-14 11:46:54 +0530

    hi

---
+++ src/Java8Practise/randomCodes/Code16.java
@@ -0,0 +1,8 @@
+package src.Java8Practise.randomCodes;
+
+public class Code16 {
+    public static void main(String[] args) {
+
+        System.out.println("This is to save my commit streak :)");
+    }
+}
