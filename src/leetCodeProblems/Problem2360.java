50672fb2726fefade6c028fb4f25ae8a58d52062
Ashu Singh <ashu.ask.singh@gmail.com>
2023-03-26 22:43:05 +0530

    leetcode question

---
+++ src/leetCodeProblems/Problem2360.java
@@ -0,0 +1,13 @@
+package src.leetCodeProblems;
+
+public class Problem2360 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int longestCycle(int[] edges) {
+        return 0;
+    }
+}
