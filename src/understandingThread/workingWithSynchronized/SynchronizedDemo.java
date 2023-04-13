d00a95d37d3f9421c4e5bf0a731d507cd68d11b5
Ashu Singh <ashu.ask.singh@gmail.com>
2023-04-13 18:19:02 +0530

    working with synchronized methods

--- src/understandingThread/workingWithSynchronized/SynchronizedDemo.java
+++ src/understandingThread/workingWithSynchronized/SynchronizedDemo.java
@@ -3,10 +3,20 @@ package src.understandingThread.workingWithSynchronized;
 public class SynchronizedDemo {
 
     public static void main(String[] args) {
-        Display d = new Display();
-        MyThread t1 = new MyThread(d, "Ashu");
-        MyThread t2 = new MyThread(d, "Akash");
+        Display d1 = new Display();
+        Display d2 = new Display();
+        MyThread t1 = new MyThread(d1, "Ashu");
+        MyThread t2 = new MyThread(d2, "Akash");
         t1.start();
         t2.start();
+
+        /**
+         *  synchronized(Display.class) {
+         *      // If a thread gets class level lock of "Display" class,
+         *      // then only it is allowed to execute
+         *      // this area.
+         *  }
+         *
+         * */
     }
 }
