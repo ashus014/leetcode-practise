4d9f3f9c9305f5830feed663c89c398b88bc9e5f
Ashu Singh <ashu.ask.singh@gmail.com>
2023-04-13 22:32:11 +0530

    synchronized blocks

--- src/understandingThread/workingWithSynchronized/Display.java
+++ src/understandingThread/workingWithSynchronized/Display.java
@@ -4,14 +4,18 @@ public class Display {
 
     public synchronized void wish(String name) {
 
-        for(int i=0; i<5; i++) {
-            System.out.print("Good Morning : ");
-
-            try{
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-            }
-            System.out.println(name);
+        // here thread has lock of Display Object
+//        Y y = new Y();
+//        synchronized (y) {
+            // here thread has lock of Display Object and y
+            for (int i = 0; i < 5; i++) {
+                System.out.print("Good Morning : ");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+                System.out.println(name);
+//            }
         }
     }
 }
