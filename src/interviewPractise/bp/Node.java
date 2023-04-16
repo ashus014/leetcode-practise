e60912acbb46c2c29ab252440b25867656db99ff
Ashu Singh <ashu.ask.singh@gmail.com>
2023-04-16 21:42:43 +0530

    Interview System Design Practise

---
+++ src/interviewPractise/bp/Node.java
@@ -0,0 +1,12 @@
+package src.interviewPractise.bp;
+
+public class Node {
+
+    Node prev;
+    Node next;
+    int key, value;
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
